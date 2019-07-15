fdf91a86193f30df6292b21a4d9c1af615142333
zfletch <zfletch2@gmail.com>
2019-07-15 13:58:25 -0400

    add navbar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,30 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, NavLink, Route, Switch } from 'react-router-dom';
-import { PerseidsHeader, PerseidsFooter } from 'perseids-react-components';
-import { NavbarToggler, Collapse } from 'reactstrap';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { PerseidsFooter } from 'perseids-react-components';
 
+import Navbar from '../Navbar';
 import Machine from '../Machine';
 import Instructions from '../Instructions';
 
 function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
-      <PerseidsHeader>
-        D'Arcy Thompson's Reasoning Machine
-
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/">
-              Home
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/instructions">
-              Instructions
-            </NavLink>
-          </li>
-        </ul>
-      </PerseidsHeader>
+      <Route path="/" component={Navbar} />
 
       <Switch>
         <Route exact path="/" component={Machine} />
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import { NavbarToggler, Collapse } from 'reactstrap';
+import { PerseidsHeader } from 'perseids-react-components';
+
+const renderCollapse = collapsed => (
+  <Collapse isOpen={!collapsed} navbar>
+    <ul className="navbar-nav mr-auto">
+      <li className="nav-item">
+        <NavLink className="nav-link" to="/">
+          Reasoning machine
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink className="nav-link" to="/instructions">
+          Instructions
+        </NavLink>
+      </li>
+    </ul>
+  </Collapse>
+);
+
+class Navbar extends Component {
+  state = {
+    collapsed: true,
+  }
+
+  toggleNavbar = () => {
+    this.setState(prevState => ({ collapsed: !prevState.collapsed }));
+  }
+
+  render() {
+    const { collapsed } = this.state;
+
+    return (
+      <>
+        <PerseidsHeader>
+          <NavbarToggler onClick={this.toggleNavbar} aria-label="navigation menu" />
+
+          {renderCollapse(collapsed)}
+        </PerseidsHeader>
+      </>
+    );
+  }
+}
+
+export default Navbar;
